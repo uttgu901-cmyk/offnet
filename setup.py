f5a7d93a08a1abd4a021b8eefffbb6aac9dd17b8
uttgu901-cmyk <uttgu.901@gmail.com>
2025-11-08 21:18:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="offnet",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "requests>=2.25.1",
-        "beautifulsoup4>=4.9.3", 
-        "lxml>=4.6.3",
-        "zeroconf>=0.28.0",
-        "python-socketio>=5.0.0",
+        "beautifulsoup4>=4.9.3",
         "rich>=10.0.0",
-        "click>=8.0.0",
     ],
     entry_points={
         'console_scripts': [
             'gloff-chat=offnet.chat.gloff_chat:main',
-            'offnet-update=offnet.core:update_all_content_cli',
+            'offnet-update=scripts.mass_parser:main',
         ],
     },
 )
