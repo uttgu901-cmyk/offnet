49e1cb22d4b16f656a803900ae387af278d250e7
uttgu901-cmyk <uttgu.901@gmail.com>
2025-11-08 22:18:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,9 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="offnet",
     version="0.1.0",
-    author="OffNet Team",
-    author_email="contact@example.com",
-    description="Offline-first Python library with local web content and mesh chat",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
     install_requires=[
         "requests>=2.25.1",
         "beautifulsoup4>=4.9.3",
